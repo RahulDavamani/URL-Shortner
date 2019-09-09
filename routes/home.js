1efcf85a436ca7971ba3af27f676a70129ff5dfe
Rahul Davamani <davamanirahul@gmail.com>
2019-09-09 18:36:02 +0530

    Remove Unwanted Variable in Home Route

--- routes/home.js
+++ routes/home.js
@@ -1,10 +1,5 @@
 const express = require('express');
 const router = express.Router();
-const validUrl = require('valid-url');
-const shortid = require('shortid');
-const config = require('config')
-
-const Url = require('../models/url')
 
 router.get('/', (req, res) => {
    res.render('home.ejs')
