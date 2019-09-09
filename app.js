337407bfc6de68d3e76a41856eba4719aa92107a
Rahul Davamani <davamanirahul@gmail.com>
2019-09-09 18:32:59 +0530

    Create Home Page and Routes

--- app.js
+++ app.js
@@ -1,10 +1,16 @@
+const bodyParser = require('body-parser')
 const express = require('express')
 const app = express();
 const PORT = 3000
-const connectDB = require('./config/db')
 
+const connectDB = require('./config/db')
 connectDB();
 
-app.use(express.json({ extented:false }))
+app.set("view engine", "ejs");
+app.use(express.static(__dirname + "/public"));
+app.use(bodyParser.urlencoded({extended: true}));
+// app.use(express.json({ extented:false }))
+
+app.use('/home', require('./routes/home'));
 
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`))
